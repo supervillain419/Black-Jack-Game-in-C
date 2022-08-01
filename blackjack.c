ef7555b528bada81a5ab3576a2cac8a0ca18da9a
supervillain419 <90406826+supervillain419@users.noreply.github.com>
2022-08-01 16:48:48 +0300

    Update blackjack.c

--- blackjack.c
+++ blackjack.c
@@ -40,44 +40,78 @@ void startGame(){
     int dealercards=0, playercards=0,r_num=0;
     int Deck[]={1,2,3,4,5,6,7,8,9,10,10,10,10}; //1-13 (11-J, 12-Q, 13-K) Cards!
     srand(time(NULL));
-    r_num += (rand() % 13)+1;
-    
-    dealersum +=Deck[r_num];
+
+    r_num = (rand() % 13);
+    dealersum += Deck[r_num];
     //printf("===> %d\n ===>%d",r_num,Deck[r_num]);
     dealercards += 1;
-    printf("\nDealers sum is: %d\n",dealersum); //Prints dealers sum!
+    printf("\nDealer Draws %d Card\n",Deck[r_num]);
+    printf("Dealers sum is: %d\n",dealersum); //Prints dealers sum!
 
     do{
-        r_num += (rand() % 13)+1;
-        playersum +=Deck[r_num];
-        playercards += 1;
-        printf("Your sum is: %d\n",playersum);
-        if(playersum <21){
-            printf("1.Hit\n2.Stay\n");
-            scanf("%d",&cho);
-            if(cho==1){
-                hit=0;
+        r_num = (rand() % 13);
+        printf("\nPlayer Draws %d Card\n",Deck[r_num]);
+        if (Deck[r_num] == 1){
+            if (playersum <= 10){
+                playersum += 11;
             }else{
-                hit=1;
+                playersum += 1;
+            }
+            
+        }else{
+            playersum += Deck[r_num];
+        }
+        
+        //playercards += 1;
+        printf("Players sum is: %d\n",playersum);
+        if(playersum < 21){
+            printf("\n==============================");
+            printf("\n1.Hit\n2.Stay\n");
+            printf("==============================\n");
+            scanf("%d",&cho);
+            if(cho == 1){
+                hit = 0;
+            }else{
+                hit = 1;
             }
         }
-    }while(playersum <21 && hit== 0);
-    printf("Players sum is: %d\n",playersum);
+    }while(playersum < 21 && hit == 0);
+    printf("\nPlayers sum is: %d\n",playersum);
 
     while (dealersum<17)
     {
         i++;
-        r_num += (rand() % 13)+1;
-        dealersum +=Deck[r_num];
+        r_num = (rand() % 13);
+        printf("\nDealer Draws %d Card\n",Deck[r_num]);
+        if (Deck[r_num] == 1){
+            if (dealersum <= 10){
+                dealersum += 11;
+            }else{
+                dealersum += 1;
+            }
+        }else{
+            dealersum += Deck[r_num];
+        }
+        //dealersum +=Deck[r_num];
         dealercards += 1;
         printf("\n%d.Dealers sum is: %d\n",i,dealersum);
     }
     printf("\n==============================");
 
+    //      CHECK ACE COUNT     //
+    
+
+
+
+
+    //      CHECK WHO WINS      //
+
     if(playersum > 21){
         printf("\n\tYou Lose!");
     }else if(playersum == dealersum){
         printf("\n\tTie!");
+    }else if(dealersum > playersum && dealersum < 22){
+        printf("\n\tYou Lose!");
     }else{
         printf("\n\tYou win!");
     }
